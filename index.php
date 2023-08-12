94c41dc57f666086e3ba5c0a8d851b616cb93476
SeefaRaafeek <136367544+SeefaRaafeek@users.noreply.github.com>
2023-08-12 21:21:04 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,303 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Responsive Personal Portfolio Website Design</title>
+
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+
+    <!--custom css-->
+    <link rel="stylesheet" href="style.css">
+
+</head>
+
+<body>
+
+    <!--header design-->
+
+    <header class="header">
+        <a href="#" class="logo">Portfolio</a>
+
+        <i class='bx bx-menu' id="menu-icon"></i>
+
+        <nav class="navbar">
+            <a href="#home" class="active">Home</a>
+            <a href="#about">About</a>
+            <a href="#services">Services</a>
+            <a href="#portfolio">Portfolio</a>
+            <a href="#contact">Contact</a>
+        </nav>
+    </header>
+
+
+    <!--home section design-->
+
+    <section class="home" id="home">
+        <div class="home-content">
+            <h3>Hello, It's Me</h3>
+            <h1>Seefa Raafeek</h1>
+            <h3>And I'm a <span class="multiple-text"></span></h3>
+            <p>Welcome to my portfolio webpage! I am a skilled and passionate web developer dedicated to creating
+                engaging and user-friendly digital experiences.</p>
+
+
+
+            <div class="social-media">
+
+                <a href="#"><i class='bx bxl-facebook'></i></a>
+                <a href="#"><i class='bx bxl-twitter'></i></a>
+                <a href="#"><i class='bx bxl-instagram-alt'></i></a>
+                <a href="#"><i class='bx bxl-linkedin'></i></a>
+            </div>
+
+            <a href="#" class="btn">Download CV</a>
+
+        </div>
+
+        <div class="home-img">
+
+            <img src="img1.jpg" alt="">
+
+        </div>
+
+    </section>
+
+    <!--about section design-->
+
+    <section class="about" id="about">
+
+        <div class="about-img">
+            <img src="img2.jpeg" alt="">
+        </div>
+
+        <div class="about-content">
+
+            <h2 class="heading">About <span>Me</span></h2>
+            <h3>Frontend Developer!</h3>
+            <p> As a web developer, I possess a comprehensive skill set that encompasses both front-end and back-end
+                development. On the front-end, I am proficient in HTML, CSS, and JavaScript, allowing me to craft
+                visually appealing and responsive websites. I pay careful attention to detail, ensuring that every
+                element of the user interface is intuitive and visually cohesive.
+
+                With a strong foundation in web development technologies and a keen eye for design, I strive to bring my
+                clients' visions to life and deliver high-quality
+                websites that leave a lasting impression.
+
+            </p>
+
+            <a href="#" class="btn">Read More</a>
+
+        </div>
+    </section>
+
+    <!--services section design-->
+    <section class="services" id="services">
+        <h2 class="heading">our <span>Services</span></h2>
+
+        <div class="services-container">
+
+            <div class="services-box">
+
+                <i class='bx bx-code-alt'></i>
+                <h3>Web Development</h3>
+                <p>Well-versed in HTML, CSS, and JavaScript, utilizing the latest frameworks and libraries to bring your
+                    vision to life.Strive for pixel-perfect desiwell-versed in HTML, CSS, and JavaScript, utilizing
+                    the latest frameworks and libraries to bring your vision to life, </p>
+
+                <a href="#" class="btn">Read More</a>
+            </div>
+
+
+
+            <div class="services-box">
+
+                <i class='bx bxs-paint'></i>
+                <h3>Graphic Design t</h3>
+                <p>Meticulously transform your visual designs into pixel-perfect websites, ensuring that every element
+                    is precisely placed and visually appealing. From color schemes and typography choices to layout and
+                    imagery.</p>
+
+                <a href="#" class="btn">Read More</a>
+            </div>
+
+
+
+            <div class="services-box">
+
+                <i class='bx bx-bar-chart-alt'></i>
+                <h3>Digital Marketing</h3>
+                <p>Specializing in creating high-performance websites that drive digital marketing success. With a
+                    strong understanding of digital marketing principles and strategies, I combine technical expertise
+                    with marketing.</p>
+
+                <a href="#" class="btn">Read More</a>
+            </div>
+
+
+        </div>
+
+    </section>
+
+    <!--Portfolio section design-->
+
+    <section class="portfolio" id="portfolio">
+        <h2 class="heading">Latest <span>Project</h2>
+
+
+        <div class="portfolio-container">
+
+            <div class="portfolio-box">
+
+                <img src="img2.jpg" alt="">
+                <div class="portfolio-layer">
+                    <h4>Web Design</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, asperiores.</p>
+
+                    <a href="#"><i class='bx bx-link-external'></i></a>
+                </div>
+
+            </div>
+
+
+            <div class="portfolio-box">
+
+                <img src="img4.jpg" alt="">
+                <div class="portfolio-layer">
+                    <h4>Web Design</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, asperiores.</p>
+
+                    <a href="#"><i class='bx bx-link-external'></i></a>
+                </div>
+
+            </div>
+
+
+            <div class="portfolio-box">
+
+                <img src="img8.jpg" alt="">
+                <div class="portfolio-layer">
+                    <h4>Web Design</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, asperiores.</p>
+
+                    <a href="#"><i class='bx bx-link-external'></i></a>
+                </div>
+
+            </div>
+
+
+            <div class="portfolio-box">
+
+                <img src="img9.jpg" alt="">
+                <div class="portfolio-layer">
+                    <h4>Web Design</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, asperiores.</p>
+
+                    <a href="#"><i class='bx bx-link-external'></i></a>
+                </div>
+
+            </div>
+
+
+
+
+
+            <div class="portfolio-box">
+
+                <img src="img10.jpeg" alt="">
+                <div class="portfolio-layer">
+                    <h4>Web Design</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, asperiores.</p>
+
+                    <a href="#"><i class='bx bx-link-external'></i></a>
+                </div>
+
+            </div>
+
+
+            <div class="portfolio-box">
+
+                <img src="img6.jpg" alt="">
+                <div class="portfolio-layer">
+                    <h4>Web Design</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, asperiores.</p>
+
+                    <a href="#"><i class='bx bx-link-external'></i></a>
+                </div>
+
+            </div>
+
+
+
+            <div class="portfolio-box">
+
+                <img src="img7.jpg" alt="">
+                <div class="portfolio-layer">
+                    <h4>Web Design</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, asperiores.</p>
+
+                    <a href="#"><i class='bx bx-link-external'></i></a>
+                </div>
+
+            </div>
+
+        </div>
+
+    </section>
+
+
+    <!--Contact section design -->
+
+    <section class="contact" id="contact">
+
+        <h2 class="heading">Contact<span>Me!</span></h2>
+
+        <form action="new.php" method="post">
+
+            <div class="input-box">
+                <input type="text" placeholder="Full Name" name="name">
+                <input type="email" placeholder="Email Address" name="email">
+            </div>
+
+
+
+            <div class="input-box">
+                <input type="number" placeholder="Mobile Number" name="mobilenumber">
+                <input type="text" placeholder="Email subject" name="emailsubject">
+            </div>
+
+            <textarea name="message" id="" cols="30" rows="10" placeholder="Your Mesage"></textarea>
+            <input type="submit" value="submit" class="btn">
+
+        </form>
+
+    </section>
+
+    <!--Footer design-->
+
+    <footer class="footer">
+        <div class="footer-text">
+            <p>Copyright &copy; 2023 by Codehal | All Rights Reserved.</p>
+        </div>
+
+        <div class="footer-iconTop">
+
+            <a href="#home"><i class='bx bx-up-arrow-alt'></i></a>
+
+        </div>
+
+    </footer>
+
+    <!--Scroll Reveal-->
+    <script src="https://unpkg.com/scrollreveal"></script>
+
+    <!--typed js-->
+    <script src="https://unpkg.com/typed.js@2.0.16/dist/typed.umd.js"></script>
+
+    <!--custom js-->
+    <script src="script.js"></script>
+
+</body>
+
+</html>
---
+++ portfolio.sql
@@ -0,0 +1,51 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Aug 12, 2023 at 03:13 PM
+-- Server version: 8.0.31
+-- PHP Version: 8.0.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `portfolio`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `clients`
+--
+
+DROP TABLE IF EXISTS `clients`;
+CREATE TABLE IF NOT EXISTS `clients` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(40) NOT NULL,
+  `email` varchar(40) NOT NULL,
+  `mobilenumber` varchar(40) NOT NULL,
+  `emailsubject` text NOT NULL,
+  `message` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `clients`
+--
+
+INSERT INTO `clients` (`id`, `name`, `email`, `mobilenumber`, `emailsubject`, `message`) VALUES
+(1, 'zahra', 'zarazaina44@gmail.com', '715538490', 'hiring', 'contact me');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
